e7cfc3dc2d0cee7e6853966f933f3319deb5019f
Guilhem SEYVET <guilhemcvdev@outlook.com>
2022-08-19 23:56:25 +0200

    fix minor bugs

--- components/Cookie.js
+++ components/Cookie.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 
 export const Cookie = () => {
-  const [cookie, setCookie] = React.useState(true);
+  const [cookie, setCookie] = useState(true);
   return (
     cookie && (
-      <div className='bg-gray-800 w-96 flex flex-col items-center justify-around fixed bottom-0 right-0 mr-2 mb-2 z-50 rounded-sm p-10'>
-        <h2 className='text-white text-center mb-5'>Salut ! c&apos;est nous les cookies ! 🍪</h2>
-        <p className='text-white text-center mb-5'>
+      <div className='bg-white w-96  flex flex-col items-center justify-around fixed bottom-0 md:right-0 md:mr-2 md:mb-2 z-50 rounded-sm p-10'>
+        <h2 className='text-gray-900 text-center mb-5'>Salut ! c&apos;est nous les cookies ! 🍪</h2>
+        <p className='text-gray-900 text-center mb-5'>
           Ce site web utilise les cookies. Ils permettent de personnaliser le
           contenu pour une navigation optimale !
         </p>
